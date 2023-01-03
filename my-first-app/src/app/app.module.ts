3b8e17e6299d09282d2134525aa4f5fb9fff8d15
git-rp <patilrahul000@gmail.com>
2023-01-03 17:55:50 +0530

    assignment for creating components

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlert } from './warningalert/warning.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, WarningAlert, SuccessAlertComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
