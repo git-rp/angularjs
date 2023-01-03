52c463c3970b34f1cf7da35874541a9de5068a14
git-rp <patilrahul000@gmail.com>
2023-01-03 17:39:21 +0530

    class name , attribute and element selector

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', //This is selecting by element
+  //selector: '[app-server]', //This is by attributes
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
