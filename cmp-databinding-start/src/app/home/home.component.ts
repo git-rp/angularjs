7e6506febfb2da905f732e11199a171a37582687
git-rp <patilrahul000@gmail.com>
2023-01-05 17:48:41 +0530

    life cycle hooks

--- cmp-databinding-start/src/app/home/home.component.ts
+++ cmp-databinding-start/src/app/home/home.component.ts
@@ -1,23 +1,39 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChange,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges {
   @Input() ninjaData: { name: string; belt: string };
   @Input() myCountry: { country: string; state: string; zip: number };
   @Output() onYell = new EventEmitter();
   @Output() onHello = new EventEmitter<{ name: string; age: number }>();
 
-  constructor() {}
+  constructor() {
+    console.log('constructor');
+  }
   cricketer = {
     name: 'tend',
     number: 1,
   };
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
   fireYellEvent(e) {
     this.onYell.emit(e);
   }
